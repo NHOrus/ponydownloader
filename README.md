127bd174db51b231b00c2f03470f888a44aefc47
NHOrus <jy6x2b32pie9@yahoo.com>
2014-01-03 14:13:50 +0400

    Attempt to read config.ini file for key to api

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ ponydownloader
 ==============
 
 Attempt to learn go and exploit derpiboo.ru public api to batch download files concurrently.
+
+
+config.ini
+==========
+
+Secion [main]
+key => your derpiboo.ru key
--- intro.go
+++ intro.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import ("fmt"
+//	"net"
+//	"os"
+//	"io"
+	"github.com/vaughan0/go-ini"
+	)	
+
 
 func main(){
-	fmt.PrintLn("Check one")
+	fmt.Println("Check one")
+	config, err := ini.LoadFile("config.ini")
+
+	if err != nil { panic(err) }
+
+	key, ok := config.Get("main", "key")
+	if !ok {
+		panic("'key' variable missing from 'main' section")
+		}
+
+	fmt.Println(key)
+//	config.Close()
 }
