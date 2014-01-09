f4c20f03e151ceec810a9dcb2f9edce46b489b46
NHOrus <jy6x2b32pie9@yahoo.com>
2014-01-09 09:55:48 +0400

    now I can input tags!

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"github.com/vaughan0/go-ini"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
-	"flag"
-	
-//	"errors"
-//	"log"
-//	"net"
-//	"crypto/sha512"
-//	"encoding/hex"
 
+	//	"errors"
+	//	"log"
+	//	"net"
+	//	"crypto/sha512"
+	//	"encoding/hex"
 )
 
 //	defaults:
 var (
-	WORKERS	int64	= 10	//Number of workers
-	IMGDIR	string	= "img"	//default download directory
+	WORKERS 	int64	= 10    //Number of workers
+	IMGDIR  	string	= "img" //default download directory
+	TAG     	string	= ""    //default string is empty, it can only ge extracted from command line
+	STARTPAGE	int		= 1		//default start page, derpiboo.ru 1-indexed
+	STOPPAGE	int		= 0		//default stop page, would stop parsing json when it ends
 	)
 
 func main() {
@@ -37,27 +39,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	//Getting stuff from config, overwriting defaults
-	
-	key, ok := config.Get("main", "key") 
+
+	key, ok := config.Get("main", "key")
 	if !ok {
 		panic("'key' variable missing from 'main' section")
 	}
-	
+
 	W_temp, _ := config.Get("main", "workers")
-		if W_temp != "" { 
-		WORKERS, err = strconv.ParseInt( W_temp, 10, 0)
-		if err != nil { fmt.Println("Wrong configuration: Amount of workers is not a number"); os.Exit(1) }
+	if W_temp != "" {
+		WORKERS, err = strconv.ParseInt(W_temp, 10, 0)
+		if err != nil {
+			fmt.Println("Wrong configuration: Amount of workers is not a number")
+			os.Exit(1)
+		}
 	}
-	
+
 	ID_temp, _ := config.Get("main", "downdir")
-		if ID_temp != "" { IMGDIR = ID_temp }
-	
+	if ID_temp != "" {
+		IMGDIR = ID_temp
+	}
+
 	//here shall be flag parser
-	
+
+	flag.StringVar(&TAG, "t", TAG, "Tag to download: Replace spaces with \"+\".")
 	flag.Parse()
-	
+
 	length := flag.NArg()
 	if length == 0 {
 		fmt.Println("Nothing to download, bye!")
@@ -65,23 +73,35 @@ func main() {
 	}
 
 	//	creating directory for downloads if not yet done
-    if err := os.MkdirAll(IMGDIR, 0777); err != nil {
-         panic(err)
-    }
+	if err := os.MkdirAll(IMGDIR, 0777); err != nil {
+		panic(err)
+	}
 
-	imgid := flag.Arg(length-1) //Last argument given presumed to be number of image on site. Temporally, because later would do with flags.
+	imgdat := make(chan Image, 10)
+	
+	if TAG == "" {
 
-	//	fmt.Println(key) //Just checking that I am not wrong
+		imgid := flag.Arg(length - 1)
 
-	_, err = strconv.ParseInt(imgid, 10, 0)
-	if err != nil { fmt.Println("Wrong input: can not parse", imgid, "as a number"); os.Exit(1) }
+		//	fmt.Println(key) //Just checking that I am not wrong
+
+		_, err = strconv.ParseInt(imgid, 10, 0)
+		if err != nil {
+			fmt.Println("Wrong input: can not parse", imgid, "as a number")
+			os.Exit(1)
+		}
+
+		fmt.Println("Processing image No " + imgid)
+
+		parseImg(imgdat, imgid, key)
+
+	}	else	{
+	//	and here we parse tag and abuse stuff horribly
+		
+		fmt.Println("trying to process tags")
+		parseTag(imgdat, TAG, key)
+	}
 	
-	
-	fmt.Println("Processing image No " + imgid)
-	
-	imgdat := make (chan Image, 1)
-	
-	parseImg (imgdat, imgid, key)
 
 	dlimage(imgdat)
 
@@ -96,9 +116,11 @@ type Image struct {
 func parseImg(imgchan chan<- Image, imgid string, key string) {
 
 	source := "http://derpiboo.ru/" + imgid + ".json?nofav=&nocomments="
-	if (  key != "" ) { source = source + "&key=" + key }
-	
-//	fmt.Println(source)
+	if key != "" {
+		source = source + "&key=" + key
+	}
+
+	//	fmt.Println(source)
 
 	resp, err := http.Get(source) //Getting our nice http response. Needs checking for 404 and other responses that are... less expected
 	if err != nil {
@@ -124,63 +146,63 @@ func parseImg(imgchan chan<- Image, imgid string, key string) {
 	imgdata.hash = dat["sha512_hash"].(string) //for the future and checking that we got file right
 	imgdata.filename = strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64) + "." + dat["file_name"].(string) + "." + dat["original_format"].(string)
 
-//	fmt.Println(strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64))
+	//	fmt.Println(strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64))
 
-//	fmt.Println(dat)
+	//	fmt.Println(dat)
 
-//	for now and troubleshooting
-//	fmt.Println(imgdata.url)
-//	fmt.Println(imgdata.hash)
-//	fmt.Println(imgdata.filename)
+	//	for now and troubleshooting
+	//	fmt.Println(imgdata.url)
+	//	fmt.Println(imgdata.hash)
+	//	fmt.Println(imgdata.filename)
 
 	imgchan <- imgdata
-	
-	return 
+
+	return
 }
 
 func dlimage(imgchan <-chan Image) {
-//	fmt.Println("reading channel")
-	
-	
+	//	fmt.Println("reading channel")
+
 	imgdata := <-imgchan
-	
+
 	fmt.Println("Saving as ", imgdata.filename)
 	PathSep, _ := strconv.Unquote(strconv.QuoteRune(os.PathSeparator))
 
 	output, err := os.Create(IMGDIR + PathSep + imgdata.filename)
-	if err !=err { panic(err)}
+	if err != err {
+		panic(err)
+	}
 	defer output.Close()
 
 	response, err := http.Get(imgdata.url)
 	if err != nil {
-		panic(err)	
+		panic(err)
 		fmt.Println("Error while downloading", imgdata.url, "-", err)
 		return
 	}
 	defer response.Body.Close()
-	
-//	hash := sha512.New()
+
+	//	hash := sha512.New()
 
 	io.Copy(output, response.Body)
 
-/*	io.Copy(hash, response.Body)
-	b := make([]byte, hash.Size())
-	hash.Sum(b[:0])
-			
-	fmt.Println("\n", hex.EncodeToString(b), "\n", imgdata.hash )
-		
-	if hex.EncodeToString(b) == imgdata.hash {
-		fmt.Println("Hash correct") 
-	}	else { 
-		fmt.Println("Hash wrong") 
-	}
+	/*	io.Copy(hash, response.Body)
+		b := make([]byte, hash.Size())
+		hash.Sum(b[:0])
 
-	fmt.Println("\n", hex.EncodeToString(hash.Sum(nil)), "\n", imgdata.hash )
-*/
+		fmt.Println("\n", hex.EncodeToString(b), "\n", imgdata.hash )
 
-}
+		if hex.EncodeToString(b) == imgdata.hash {
+			fmt.Println("Hash correct")
+		}	else {
+			fmt.Println("Hash wrong")
+		}
 
-func parseTag ( imgchan chan<- Image, tag string, key string) {
+		fmt.Println("\n", hex.EncodeToString(hash.Sum(nil)), "\n", imgdata.hash )
+	*/
 
 }
 
+func parseTag(imgchan chan<- Image, tag string, key string) {
+
+}
