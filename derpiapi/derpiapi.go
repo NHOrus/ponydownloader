33696e81b2223387093ba1c41f62d4add5de7955
NHOrus <jy6x2b32pie9@yahoo.com>
2014-05-08 20:25:35 +0400

    What's double nested function between friends?

--- derpiapi/derpiapi.go
+++ derpiapi/derpiapi.go
@@ -27,8 +27,7 @@ func InfoToChannel(dat map[string]interface{}, imgchan chan<- Image) {
 	imgdata.Url = "http:" + dat["image"].(string)
 	//	imgdata.Hashval = dat["sha512_hash"].(string)
 	if dat["original_format"].(string) == "svg" {
-		tmp := strings.Split(dat["image"].(string), "/")
-		imgdata.Url = "https://derpicdn.net/img/download/" + strings.Join(tmp[5:8], "/") + "/" + strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64) + ".svg" //Was afraid to extract things I needed from the date field, so extracting them from URL.
+		imgdata.Url = "https://derpicdn.net/img/download/" + strings.Join(strings.Split(dat["image"].(string), "/")[5:8], "/") + "/" + strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64) + ".svg" //Was afraid to extract things I needed from the date field, so extracting them from URL.
 	}
 	imgdata.Filename = (strconv.FormatFloat(dat["id_number"].(float64), 'f', -1, 64) + "." + dat["file_name"].(string) + "." + dat["original_format"].(string))
 	imgdata.Imgid = int(dat["id_number"].(float64))
