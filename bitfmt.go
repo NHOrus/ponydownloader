0766d9d591e2dab9dc693e72ed2f123a387823ea
NHOrus <jy6x2b32pie9@yahoo.com>
2015-08-25 22:49:18 +0300

    Going from lots of if to switch
    Adding a panic for panic. b can't be less than zero, but if something
    messes up...

--- bitfmt.go
+++ bitfmt.go
@@ -10,24 +10,24 @@ const (
 	MiB         = 1024 * 1024
 	GiB         = 1024 * 1024 * 1024
 	TiB         = 1024 * 1024 * 1024 * 1024
-	PiB         = 1024 * 1024 * 1024 * 1024 * 1024
+	PiB         = 1024 * 1024 * 1024 * 1024 * 1024 //This may be too futureproof
 )
 
 func fmtbytes(b float64) string {
-	if b > PiB {
+	switch {
+	case b < 0:
+		panic("Natural number is less than zero. Stuff is wrong")
+	case b > PiB:
 		return fmt.Sprintf("%.2f PiB", b/PiB)
-	}
-	if b > TiB {
+	case b > TiB:
 		return fmt.Sprintf("%.2f TiB", b/TiB)
-	}
-	if b > GiB {
+	case b > GiB:
 		return fmt.Sprintf("%.2f GiB", b/GiB)
-	}
-	if b > MiB {
+	case b > MiB:
 		return fmt.Sprintf("%.2f MiB", b/MiB)
-	}
-	if b > KiB {
+	case b > KiB:
 		return fmt.Sprintf("%.2f KiB", b/KiB)
+	default:
+		return fmt.Sprintf("%.0f B", b)
 	}
-	return fmt.Sprintf("%.0f B", b)
 }
