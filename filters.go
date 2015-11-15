57dc2abc56a6f7720e8e54113e589a8cab584dc0
NHOrus <jy6x2b32pie9@yahoo.com>
2015-11-15 09:32:28 +0300

    Rebuild filters architecture in attempt to get rid of global options

--- filters.go
+++ filters.go
@@ -2,24 +2,47 @@ package main
 
 import "log"
 
-//FilterChannel cuts off unneeded images
-func FilterChannel(in ImageCh) (out ImageCh) {
+type Filter func(ImageCh) ImageCh
+
+var filters []Filter
+
+func filterInit(opts Options) {
 	if !opts.Filter {
 		log.Println("Filter is off")
-		return in
+		filters = append(filters, nopFilter)
+		return
 	}
 	log.Println("Filter is on")
-	out = make(ImageCh, 1)
-	go func() {
-		for imgdata := range in {
+	filters = append(filters, scoreFilterGenerator(opts))
+}
 
-			if imgdata.Score >= opts.Score {
-				out <- imgdata
-				continue
+func nopFilter(in ImageCh) ImageCh {
+	return in
+}
+
+func scoreFilterGenerator(option Options) Filter {
+	return func(in ImageCh) ImageCh {
+		out := make(ImageCh, 1)
+		go func() {
+			for imgdata := range in {
+
+				if imgdata.Score >= option.Score {
+					out <- imgdata
+					continue
+				}
+				log.Println("Filtering " + imgdata.Filename)
 			}
-			log.Println("Filtering " + imgdata.Filename)
-		}
-		close(out)
-	}()
-	return out
+			close(out)
+		}()
+		return out
+	}
+}
+
+//FilterChannel cuts off unneeded images
+func FilterChannel(in ImageCh) (out ImageCh) {
+	out = in
+	for _, filter := range filters {
+		out = filter(out)
+	}
+	return
 }
