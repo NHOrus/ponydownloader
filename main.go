0c7e94a7171fc7690fd8a680de8adae558534236
NHOrus <jy6x2b32pie9@yahoo.com>
2015-11-15 11:44:07 +0300

    Version bump

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Derpiboo.ru Downloader version 0.5.0")
+	fmt.Println("Derpiboo.ru Downloader version 0.5.2")
 
 	args, iniopts := configSetup(&opts)
 
