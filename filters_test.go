9873ea890909cebcb5d3ba8931f92f6a33e6cb6a
NHOrus <jy6x2b32pie9@yahoo.com>
2016-04-14 13:32:56 +0300

    Test for FilterChannel implementation
    More bits for #14, more coverage

--- filters_test.go
+++ filters_test.go
@@ -59,3 +59,26 @@ func TestFilterAlwaysFalse(t *testing.T) {
 		t.Error("Filter remains unexpectedly open")
 	}
 }
+
+func TestFilterChannel(t *testing.T) {
+	filterInit(&FiltOpts{ScoreF: true, FavesF: true}, false)
+
+	in := make(ImageCh, 3)
+	out := FilterChannel(in)
+	in <- Image{Score: -1}
+	in <- Image{Faves: -1}
+	in <- Image{Imgid: 1}
+
+	close(in)
+	pass, _ := <-out
+
+	if (pass != Image{Imgid: 1}) {
+		t.Error("Incorrect work of the filter, passed ", pass, "instead of ", Image{Imgid: 1})
+	}
+
+	pass, ok := <-out
+
+	if ok && (pass != Image{}) {
+		t.Error("Wat is going on with FilterChannel?")
+	}
+}
